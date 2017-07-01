1203ef7d47a672ffae8a49b943ee81c841ddff33
Edward Seley <edwardseley@gmail.com>
2017-07-01 17:37:01 -0500

    Added Merge-Sort

--- Algorithms.py
+++ Algorithms.py
@@ -4,8 +4,8 @@ class Algorithm:
 
     def __init__(self, AlgorithmNum, sequence):
 
-        options = {0 : lambda: self.insertionSort(sequence, True),
-                   #1 : lambda: self.insertionSort(sequence, True),
+        options = {0 : lambda: self.insertionSort(sequence),
+                   1 : lambda: self.merge_sort(sequence, 0, len(sequence)),
                    #4: sqr,
                    #9: sqr,
                    #2: even,
@@ -16,9 +16,7 @@ class Algorithm:
 
         options[AlgorithmNum]()
 
-    def insertionSort(self, sequence, increasing):
-        if type(increasing) != bool:
-            raise ValueError("parameter must be a boolean value, not: " + type(increasing))
+    def insertionSort(self, sequence):
         for x in range(1, len(sequence)):
             key = sequence[x]
             j = x - 1
@@ -28,9 +26,49 @@ class Algorithm:
             sequence[j + 1] = key
         return sequence
 
+    def merge_sort(self, sequence, first, last):
+        if first < last:
+            middle = int(0.5 * (first + last))
+            self.merge_sort(sequence, first, middle)
+            self.merge_sort(sequence, middle + 1, last)
+            self.merge(sequence, first, middle, last)
+            print(sequence)
+
+    def merge(self, sequence, first, middle, last):
+        subOneLength = middle - first
+        subTwoLength = last - middle
+        leftSequence = [None] * (subOneLength + 1)
+        rightSequence = [None] * (subTwoLength + 1)
+        for x in range(subOneLength):
+            leftSequence[x] = sequence[first + x]
+        for y in range(subTwoLength):
+            rightSequence[y] = sequence[middle + y]
+
+        leftSequence[subOneLength] = 1000
+        rightSequence[subTwoLength] = 1000
+
+        print("subOneLength: " + str(subOneLength))
+        print("subTwoLength: " + str(subTwoLength))
+        print("left sequence: " + str(leftSequence))
+        print("right sequence: " + str(rightSequence))
+
+        x = 0
+        y = 0
+
+        for i in range(first, last):
+            if leftSequence[x] < rightSequence[y]:
+                sequence[i] = leftSequence[x]
+                x = x + 1
+            else:
+                sequence[i] = rightSequence[y]
+                y = y + 1
+
 if __name__ == "__main__":
-    analysis = AlgorithmAnalysis()
-    performTimeList = analysis.timeAlgorithm(0, 5000)
+    #analysis = AlgorithmAnalysis()
+    #performTimeList = analysis.timeAlgorithm(0, 1000)
+    random_list = [3, 7, 1, 9, 3, 6, 4, 12]
+    Algorithm(1, random_list)
+
 
 
 
